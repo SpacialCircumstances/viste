543dc570538f59f23c3ab115cf816aa2b877f0dc
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-12-20 23:15:22 +0100

    Add listeners struct for storing multiple listeners

--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,41 @@ use std::cell::Cell;
 
 type Listener<'a, T> = Box<dyn Fn(&T) -> () + 'a>;
 
-fn map<'a, F, T: 'a, M: Fn(&F) -> T + 'a>(mapper: M, listeners: Vec<Listener<'a, T>>) -> Listener<'a, F> {
+struct Listeners<'a, T>(Vec<Listener<'a, T>>);
+
+impl<'a, T> Listeners<'a, T> {
+    pub fn new() -> Self {
+        Listeners(Vec::with_capacity(1))
+    }
+
+    pub fn notify_all(&self, data: &T) {
+        self.0.iter().for_each(|l| (l)(data));
+    }
+}
+
+fn map<'a, F, T: 'a, M: Fn(&F) -> T + 'a>(mapper: M, listeners: Listeners<'a, T>) -> Listener<'a, F> {
     return Box::new(move |f| {
         let t = mapper(f);
-        for l in &listeners {
-            (l)(&t)
-        }
+        listeners.notify_all(&t)
     })
 }
 
-fn filter<'a, T: 'a, F: Fn(&T) -> bool + 'a>(filter: F, listeners: Vec<Listener<'a, T>>) -> Listener<'a, T> {
+fn filter<'a, T: 'a, F: Fn(&T) -> bool + 'a>(filter: F, listeners: Listeners<'a, T>) -> Listener<'a, T> {
     return Box::new(move |t| {
         if filter(t) {
-            for l in &listeners {
-                (l)(t)
-            }
+            listeners.notify_all(t);
         }
     })
 }
 
-fn cache<'a, T: Copy + Eq + 'a>(listeners: Vec<Listener<'a, T>>) -> Listener<'a, T> {
+fn cache<'a, T: Copy + Eq + 'a>(listeners: Listeners<'a, T>) -> Listener<'a, T> {
     let mut cached: Cell<Option<T>> = Cell::new(None);
     return Box::new(move |t| {
         match &cached.get() {
             Some(old) if old == t => (),
             _ => {
                 cached.replace(Some(*t));
-                for l in &listeners {
-                    (l)(t)
-                }
+                listeners.notify_all(t);
             }
         }
     })
@@ -44,10 +50,8 @@ fn push<T>(value: T, listener: Listener<T>) {
     (listener)(&value)
 }
 
-fn connect<'a, T: 'a>(listeners: Vec<Listener<'a, T>>) -> Listener<'a, T> {
+fn connect<'a, T: 'a>(listeners: Listeners<'a, T>) -> Listener<'a, T> {
     return Box::new(move |t| {
-        for l in &listeners {
-            (l)(t)
-        }
+        listeners.notify_all(t);
     })
 }
