e4a36f5657592afedffb1e7c19fd014618312f62
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-12-20 14:03:09 +0100

    Combinator-based listeners

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,32 @@
-trait Listener<T> {
-    fn invoke(&mut self, param: &T);
+type Listener<'a, T> = Box<dyn Fn(&T) -> () + 'a>;
+
+fn map<'a, F, T: 'a, M: Fn(&F) -> T + 'a>(mapper: M, listeners: Vec<Listener<'a, T>>) -> Listener<'a, F> {
+    return Box::new(move |f| {
+        let t = mapper(f);
+        for l in &listeners {
+            (l)(&t)
+        }
+    })
 }
 
-trait Observable<T> {
-    fn subscribe<>(&mut self, listener: dyn Listener<T>);
+fn filter<'a, T: 'a, F: Fn(&T) -> bool + 'a>(filter: F, listeners: Vec<Listener<'a, T>>) -> Listener<'a, T> {
+    return Box::new(move |t| {
+        if filter(t) {
+            for l in &listeners {
+                (l)(t)
+            }
+        }
+    })
 }
 
-impl<T, F: Fn(&T) -> ()> Listener<T> for F {
-    fn invoke(&mut self, param: &T) {
-        (self)(param)
-    }
+fn push<T>(value: T, listener: Listener<T>) {
+    (listener)(&value)
+}
+
+fn connect<'a, T: 'a>(listeners: Vec<Listener<'a, T>>) -> Listener<'a, T> {
+    return Box::new(move |t| {
+        for l in &listeners {
+            (l)(t)
+        }
+    })
 }
