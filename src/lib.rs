029742f769eaaca5241a584f6e180685bfb4f0b7
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-12-20 12:48:06 +0100

    Simple callback implementation

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,9 @@ trait Listener<T> {
 trait Observable<T> {
     fn subscribe<>(&mut self, listener: dyn Listener<T>);
 }
+
+impl<T, F: Fn(&T) -> ()> Listener<T> for F {
+    fn invoke(&mut self, param: &T) {
+        (self)(param)
+    }
+}
