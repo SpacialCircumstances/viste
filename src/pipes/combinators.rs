677d3e8f497bb3c8b280e746a230f58534aefd91
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-12-21 19:22:16 +0100

    Rename listeners to pipes and add combinator module

---
+++ src/pipes/combinators.rs
@@ -0,0 +1,81 @@
+use std::cell::{Cell, RefCell};
+use std::ops::DerefMut;
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+use std::rc::Rc;
+use crate::pipes::{Pipe, Pipes};
+
+pub fn map<'a, F, T: 'a, M: Fn(&F) -> T + 'a>(mapper: M, pipes: Pipes<'a, T>) -> Pipe<'a, F> {
+    Pipe::new(move |f| {
+        let t = mapper(f);
+        pipes.notify_all(&t)
+    })
+}
+
+pub fn filter<'a, T: 'a, F: Fn(&T) -> bool + 'a>(filter: F, pipes: Pipes<'a, T>) -> Pipe<'a, T> {
+    Pipe::new(move |t| {
+        if filter(t) {
+            pipes.notify_all(t);
+        }
+    })
+}
+
+pub fn cache<'a, T: Copy + Eq + 'a>(pipes: Pipes<'a, T>) -> Pipe<'a, T> {
+    let cached: Cell<Option<T>> = Cell::new(None);
+    Pipe::new(move |t| {
+        match &cached.get() {
+            Some(old) if old == t => (),
+            _ => {
+                cached.replace(Some(*t));
+                pipes.notify_all(t);
+            }
+        }
+    })
+}
+
+pub fn cache_clone<'a, T: Clone + Eq + 'a>(pipes: Pipes<'a, T>) -> Pipe<'a, T> {
+    let cached: RefCell<Option<T>> = RefCell::new(None);
+    Pipe::new(move |t| {
+        match cached.borrow_mut().deref_mut() {
+            Some(old) if old == t => (),
+            x => {
+                *x = Some(t.clone());
+                pipes.notify_all(t);
+            }
+        };
+    })
+}
+
+pub fn cache_hash<'a, T: Hash + 'a>(pipes: Pipes<'a, T>) -> Pipe<'a, T> {
+    let cached: Cell<Option<u64>> = Cell::new(None);
+    Pipe::new(move |t: &T| {
+        let mut hasher = DefaultHasher::new();
+        t.hash(&mut hasher);
+        let new_hash = hasher.finish();
+        match cached.get() {
+            Some(old_hash) if old_hash == new_hash => (),
+            _ => {
+                cached.set(Some(new_hash));
+                pipes.notify_all(t);
+            }
+        }
+    })
+}
+
+pub fn store<'a, T: Copy + 'a>(default: T) -> (Pipe<'a, T>, Rc<Cell<T>>) {
+    let store = Rc::new(Cell::new(default));
+    let c = store.clone();
+    let pipe = Pipe::new(move |t| {
+        c.set(*t)
+    });
+    (pipe, store)
+}
+
+pub fn store_clone<'a, T: Clone + 'a>(default: T) -> (Pipe<'a, T>, Rc<RefCell<T>>) {
+    let store = Rc::new(RefCell::new(default));
+    let c = store.clone();
+    let pipe = Pipe::new(move |t: &T| {
+        c.replace(t.clone());
+    });
+    (pipe, store)
+}
--- src/pipes/mod.rs
+++ src/pipes/mod.rs
@@ -1,1 +1,53 @@
-pub mod listener;
+use std::rc::Rc;
+
+pub mod combinators;
+
+pub struct Pipe<'a, T>(Box<dyn Fn(&T) -> () + 'a>);
+
+impl<'a, T> Pipe<'a, T> {
+    pub fn new<F: Fn(&T) -> () + 'a>(fun: F) -> Self {
+        Self(Box::new(fun))
+    }
+
+    pub fn invoke(&self, data: &T) {
+        (self.0)(data)
+    }
+
+    pub fn cloneable(self) -> Rc<Self> {
+        Rc::new(self)
+    }
+}
+
+impl<'a, T: 'a> From<Rc<Pipe<'a, T>>>  for Pipe<'a, T> {
+    fn from(l: Rc<Pipe<'a, T>>) -> Self {
+        Pipe::new(move |t| l.invoke(t))
+    }
+}
+
+impl<'a, T: 'a> From<Pipes<'a, T>> for Pipe<'a, T> {
+    fn from(l: Pipes<'a, T>) -> Self {
+        Pipe::new(move |t| l.notify_all(t))
+    }
+}
+
+pub struct Pipes<'a, T>(Vec<Pipe<'a, T>>);
+
+impl<'a, T> Pipes<'a, T> {
+    pub fn new() -> Self {
+        Self(Vec::with_capacity(1))
+    }
+
+    pub fn single(pipe: Pipe<'a, T>) -> Self {
+        Pipes(vec![ pipe ])
+    }
+
+    pub fn notify_all(&self, data: &T) {
+        self.0.iter().for_each(|l| l.invoke(data));
+    }
+}
+
+impl<'a, T> From<Pipe<'a, T>> for Pipes<'a, T> {
+    fn from(p: Pipe<'a, T>) -> Self {
+        Pipes::single(p)
+    }
+}
