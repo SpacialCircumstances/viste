932ed3d7f20881e727420e05089a16dd318f3dff
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-12-26 19:47:04 +0100

    Put values into module and reexport them

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::rc::Rc;
 use std::cell::{Ref, RefCell};
 use std::ops::Deref;
-use std::marker::PhantomData;
+pub use crate::values::*;
 
 pub mod wires;
+pub mod values;
 
 pub struct RWire<'a, T>(Box<dyn Fn(&T) -> () + 'a>);
 
@@ -32,7 +33,7 @@ impl<'a, T: Copy + 'a> RWire<'a, T> {
         let pipe = RWire::new(move |t| {
             c.replace(*t);
         });
-        (pipe, OwnedRValue(store))
+        (pipe, OwnedRValue::new(store))
     }
 }
 
@@ -43,7 +44,7 @@ impl<'a, T: Clone + 'a> RWire<'a, T> {
         let pipe = RWire::new(move |t: &T| {
             c.replace(t.clone());
         });
-        (pipe, OwnedRValue(store))
+        (pipe, OwnedRValue::new(store))
     }
 }
 
@@ -93,44 +94,6 @@ impl<'a, T> Deref for RefWrapper<'a, T> {
 
 pub trait RValue<T>: Clone {
     fn data(&self) -> RefWrapper<T>;
-}
-
-pub trait RValueExt<T>: RValue<T> {
-    fn select<U, M: Fn(&T) -> &U + Clone>(&self, mapper: M) -> SelectRValue<T, U, M, Self> {
-        SelectRValue(self.clone(), mapper, PhantomData::default())
-    }
-}
-
-impl<T, R: RValue<T>> RValueExt<T> for R {
-
-}
-
-pub struct OwnedRValue<T>(Rc<RefCell<T>>);
-
-impl<T> Clone for OwnedRValue<T> {
-    fn clone(&self) -> Self {
-        Self(self.0.clone())
-    }
-}
-
-impl<T> RValue<T> for OwnedRValue<T> {
-    fn data(&self) -> RefWrapper<T> {
-        RefWrapper(self.0.borrow())
-    }
-}
-
-pub struct SelectRValue<T, T2, M: Fn(&T) -> &T2 + Clone, R: RValue<T>>(R, M, PhantomData<T>);
-
-impl<T, T2, M: Fn(&T) -> &T2 + Clone, R: RValue<T>> Clone for SelectRValue<T, T2, M, R> {
-    fn clone(&self) -> Self {
-        Self(self.0.clone(), self.1.clone(), PhantomData::default())
-    }
-}
-
-impl<T, T2, M: Fn(&T) -> &T2 + Clone, R: RValue<T>> RValue<T2> for SelectRValue<T, T2, M, R> {
-    fn data(&self) -> RefWrapper<T2> {
-        RefWrapper(Ref::map(self.0.data().0, &self.1))
-    }
 }
 
 pub struct RStream<'a, T>(Box<dyn Fn(T) -> () + 'a>);
