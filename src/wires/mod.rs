6cf06e6c90d61698aa2affae09a53f23a91d0cf4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-12-29 16:23:44 +0100

    Add test for wire map

--- src/wires/mod.rs
+++ src/wires/mod.rs
@@ -1,1 +1,18 @@
 pub mod combinators;
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use crate::wires::combinators::map;
+
+    #[test]
+    fn test_map() {
+        let (wire, res) = RWire::store(None);
+        let mapped = map(|x| Some(x + 1), wire.into());
+        assert!(res.data().is_none());
+        mapped.run(&1);
+        assert_eq!(*res.data(), Some(2));
+        mapped.run(&3);
+        assert_eq!(*res.data(), Some(4));
+    }
+}
