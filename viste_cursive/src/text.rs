1579f40fc5a1b3f54bff2b97038daf4b0ffe4306
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-04 19:25:20 +0100

    Create a basic reactive text view

--- viste_cursive/src/text.rs
+++ viste_cursive/src/text.rs
@@ -1,12 +1,10 @@
 use cursive::views::TextContent;
 use viste_reactive::RWire;
 
-pub fn appending(text: &TextContent) -> RWire<String> {
-    let text = text.clone();
+pub fn appending<'a>(text: TextContent) -> RWire<'a, String> {
     RWire::new(move |s| text.append(s))
 }
 
-pub fn setting(text: &TextContent) -> RWire<String> {
-    let text = text.clone();
+pub fn setting<'a>(text: TextContent) -> RWire<'a, String> {
     RWire::new(move |s| text.set_content(s))
 }
--- viste_cursive/src/views.rs
+++ viste_cursive/src/views.rs
@@ -1,7 +1,50 @@
-use cursive::view::View;
+use crate::text::setting;
+use cursive::view::{View, ViewWrapper};
+use cursive::views::{TextContent, TextView};
+use cursive::wrap_impl;
+use viste_reactive::RWire;
 
 pub trait RView: View {
     type Bindings;
 
     fn bindings(&self) -> &Self::Bindings;
+}
+
+pub struct TextBindings {
+    pub content: RWire<'static, String>,
+}
+
+pub struct RTextView {
+    view: TextView,
+    bindings: TextBindings,
+}
+
+impl RTextView {
+    pub fn new() -> Self {
+        let tcontent = TextContent::new("");
+        let content = setting(tcontent.clone());
+        let bindings = TextBindings { content };
+        Self {
+            view: TextView::new_with_content(tcontent),
+            bindings,
+        }
+    }
+}
+
+impl Default for RTextView {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ViewWrapper for RTextView {
+    wrap_impl!(self.view: TextView);
+}
+
+impl RView for RTextView {
+    type Bindings = TextBindings;
+
+    fn bindings(&self) -> &Self::Bindings {
+        &self.bindings
+    }
 }
