73b01cbc7f3baa24bc435d831c7a47d1950e7e8d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-07 17:02:31 +0100

    Add sending to list stream via sender object

--- viste_reactive/src/lists.rs
+++ viste_reactive/src/lists.rs
@@ -10,3 +10,35 @@ pub enum ListDelta<T> {
 }
 
 pub type ListStream<'a, T> = RStream<'a, ListDelta<T>>;
+
+pub struct StreamingListSender<'a, T>(ListStream<'a, T>);
+
+impl<'a, T> StreamingListSender<'a, T> {
+    pub fn new(stream: ListStream<'a, T>) -> Self {
+        Self(stream)
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.0.push(ListDelta::Add(value));
+    }
+
+    pub fn clear(&mut self) {
+        self.0.push(ListDelta::Clear);
+    }
+
+    pub fn remove(&mut self, value: T) {
+        self.0.push(ListDelta::Remove(value));
+    }
+
+    pub fn insert(&mut self, index: usize, element: T) {
+        self.0.push(ListDelta::Insert(index, element));
+    }
+
+    pub fn remove_at(&mut self, index: usize) {
+        self.0.push(ListDelta::RemoveAt(index))
+    }
+
+    pub fn swap(&mut self, i1: usize, i2: usize) {
+        self.0.push(ListDelta::Swap(i1, i2))
+    }
+}
