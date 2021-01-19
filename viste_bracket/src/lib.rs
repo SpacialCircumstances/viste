86253dcfefae7e5a197ade53684bf4e03bbe026a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-19 18:03:32 +0100

    Add very basic state for bracket

--- viste_bracket/src/lib.rs
+++ viste_bracket/src/lib.rs
@@ -1,1 +1,17 @@
+use bracket_lib::prelude::{BTerm, GameState};
 
+pub trait Component {
+    fn draw(&mut self, ctx: &mut BTerm);
+}
+
+type ComponentBox = Box<dyn Component>;
+
+pub struct ReactiveState {
+    root: ComponentBox,
+}
+
+impl GameState for ReactiveState {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        self.root.draw(ctx);
+    }
+}
