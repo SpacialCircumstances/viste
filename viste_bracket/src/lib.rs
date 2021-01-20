a97bdcedb43dfa419a02f6fa315e696b45caf0e4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-20 18:49:17 +0100

    Add event handling via stream

--- viste_bracket/src/lib.rs
+++ viste_bracket/src/lib.rs
@@ -1,4 +1,4 @@
-use bracket_lib::prelude::{BTerm, Console, GameState};
+use bracket_lib::prelude::{BTerm, Console, GameState, VirtualKeyCode};
 use std::rc::Rc;
 use viste_reactive::{OwnedRValue, RStream, RValue, RWire, RWires};
 
@@ -49,18 +49,62 @@ impl<'a> Component for TextComponent<'a> {
     }
 }
 
+pub enum Event {
+    KeyPressed(VirtualKeyCode),
+    KeyReleased(VirtualKeyCode),
+    LeftMouseDown(i32, i32),
+    LeftMouseUp(i32, i32),
+    MouseMoved { from: (i32, i32), to: (i32, i32) },
+}
+
 pub struct ReactiveState {
     root: Rc<dyn Component>,
+    event_stream: RStream<'static, Event>,
+    last_key: Option<VirtualKeyCode>,
+    last_mouse: (i32, i32),
+    last_left_click: bool,
 }
 
 impl ReactiveState {
-    pub fn new(root: Rc<dyn Component>) -> Self {
-        Self { root }
+    pub fn new(root: Rc<dyn Component>, event_stream: RStream<'static, Event>) -> Self {
+        Self {
+            root,
+            event_stream,
+            last_key: None,
+            last_mouse: (0, 0),
+            last_left_click: false,
+        }
     }
 }
 
 impl GameState for ReactiveState {
     fn tick(&mut self, ctx: &mut BTerm) {
+        match (self.last_key, ctx.key) {
+            (None, Some(k)) => self.event_stream.push(Event::KeyPressed(k)),
+            (Some(k), None) => self.event_stream.push(Event::KeyReleased(k)),
+            _ => (),
+        };
+        self.last_key = ctx.key;
+
+        if self.last_mouse != ctx.mouse_pos {
+            self.event_stream.push(Event::MouseMoved {
+                from: self.last_mouse,
+                to: ctx.mouse_pos,
+            });
+            self.last_mouse = ctx.mouse_pos;
+        }
+
+        match (self.last_left_click, ctx.left_click) {
+            (true, false) => self
+                .event_stream
+                .push(Event::LeftMouseUp(ctx.mouse_pos.0, ctx.mouse_pos.1)),
+            (false, true) => self
+                .event_stream
+                .push(Event::LeftMouseDown(ctx.mouse_pos.0, ctx.mouse_pos.1)),
+            _ => (),
+        };
+        self.last_left_click = ctx.left_click;
+
         self.root.draw(ctx);
     }
 }
