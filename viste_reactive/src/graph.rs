63d34f6d776425b92ada992d8b99a6032260d393
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-02-19 23:34:41 +0100

    Store empty graph nodes in seperate list to avoid expensive searching

--- viste_reactive/src/graph.rs
+++ viste_reactive/src/graph.rs
@@ -1,3 +1,4 @@
+use std::collections::vec_deque::VecDeque;
 use std::mem::replace;
 use std::ops::Index;
 
@@ -10,33 +11,33 @@ struct Adjacency {
 }
 
 enum Node<T> {
-    Empty(Option<usize>),
+    Empty,
     Filled(T, Adjacency),
 }
 
 pub struct Graph<T> {
-    first_empty_index: usize,
     nodes: Vec<Node<T>>,
+    free_nodes: VecDeque<usize>,
 }
 
 impl<T> Graph<T> {
     pub fn new() -> Self {
         Graph {
-            first_empty_index: 0,
             nodes: vec![],
+            free_nodes: VecDeque::new(),
         }
     }
 
     fn get_adjacency_mut(&mut self, node: usize) -> &mut Adjacency {
         match &mut self.nodes[node] {
-            Node::Empty(_) => panic!("Expected filled node"),
+            Node::Empty => panic!("Expected filled node"),
             Node::Filled(_, adj) => adj,
         }
     }
 
     fn get_adjacency(&self, node: usize) -> &Adjacency {
         match &self.nodes[node] {
-            Node::Empty(_) => panic!("Expected filled node"),
+            Node::Empty => panic!("Expected filled node"),
             Node::Filled(_, adj) => adj,
         }
     }
@@ -49,23 +50,19 @@ impl<T> Graph<T> {
                 children: Vec::new(),
             },
         );
-        let res_idx = NodeIndex(self.first_empty_index);
-        if self.first_empty_index >= self.nodes.len() {
-            self.nodes.push(node);
-            self.first_empty_index += 1;
-        } else {
-            match self.nodes[self.first_empty_index] {
-                Node::Filled(_, _) => panic!("Expected empty node"),
-                Node::Empty(next) => {
-                    self.nodes[self.first_empty_index] = node;
-                    match next {
-                        None => self.first_empty_index = self.nodes.len(),
-                        Some(next) => self.first_empty_index = next,
-                    }
-                }
+        match self.free_nodes.pop_front() {
+            None => {
+                self.nodes.push(node);
+                NodeIndex(self.nodes.len())
             }
+            Some(free_idx) => match self.nodes[free_idx] {
+                Node::Filled(_, _) => panic!("Expected empty node"),
+                Node::Empty => {
+                    self.nodes[free_idx] = node;
+                    NodeIndex(free_idx)
+                }
+            },
         }
-        res_idx
     }
 
     pub fn add_edge(&mut self, from: NodeIndex, to: NodeIndex) {
@@ -79,8 +76,7 @@ impl<T> Graph<T> {
     }
 
     pub fn remove_node(&mut self, node: NodeIndex) -> T {
-        //TODO: Find next empty node
-        match replace(&mut self.nodes[node.0], Node::Empty(None)) {
+        match replace(&mut self.nodes[node.0], Node::Empty) {
             Node::Filled(data, adj) => {
                 for ch in &adj.children {
                     self.get_adjacency_mut(*ch).parents.remove(node.0);
@@ -88,9 +84,10 @@ impl<T> Graph<T> {
                 for p in &adj.parents {
                     self.get_adjacency_mut(*p).children.remove(node.0);
                 }
+                self.free_nodes.push_back(node.0);
                 data
             }
-            Node::Empty(_) => panic!("Expected filled node"),
+            Node::Empty => panic!("Expected filled node"),
         }
     }
 }
@@ -101,7 +98,7 @@ impl<T> Index<NodeIndex> for Graph<T> {
     fn index(&self, index: NodeIndex) -> &Self::Output {
         match &self.nodes[index.0] {
             Node::Filled(val, _) => val,
-            Node::Empty(_) => panic!("Expected filled node"),
+            Node::Empty => panic!("Expected filled node"),
         }
     }
 }
