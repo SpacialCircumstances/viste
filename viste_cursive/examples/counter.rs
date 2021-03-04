717c5fac23d8d40f5e7228540439fd061f4fafed
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-03-04 19:22:21 +0100

    Static hello world text with signals

--- viste_cursive/examples/counter.rs
+++ viste_cursive/examples/counter.rs
@@ -1,10 +1,15 @@
 use cursive::views::{Dialog, TextView};
+use viste_cursive::RTextView;
+use viste_reactive::signals::World;
 
 fn main() {
+    let world = World::new();
+    let (set_text, current_text) = world.mutable(String::from("Hello World!"));
+
     let mut siv = cursive::default();
 
     siv.add_layer(
-        Dialog::around(TextView::new("Hello Dialog!"))
+        Dialog::around(RTextView::new(current_text))
             .title("Cursive")
             .button("Quit", |s| s.quit()),
     );
