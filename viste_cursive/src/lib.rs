831432decc85eb682ada9325bf20444564937a01
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-03-04 19:22:10 +0100

    Create a reactive text view based on a signal

--- viste_cursive/src/lib.rs
+++ viste_cursive/src/lib.rs
@@ -1,7 +1,46 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use cursive::view::ViewWrapper;
+use cursive::views::{TextContent, TextView};
+use cursive::{Printer, Vec2, View, With};
+use viste_reactive::signals::Node;
+
+pub struct RTextView {
+    view: TextView,
+    text: Node<'static, String>,
+    content: TextContent,
+}
+
+impl RTextView {
+    pub fn new(text: Node<'static, String>) -> Self {
+        let content = text.with_data(|t, _| TextContent::new(t));
+
+        let view = TextView::new_with_content(content.clone());
+        Self {
+            view,
+            text,
+            content,
+        }
+    }
+}
+
+impl ViewWrapper for RTextView {
+    type V = TextView;
+
+    fn with_view<F, R>(&self, f: F) -> Option<R>
+    where
+        F: FnOnce(&Self::V) -> R,
+    {
+        Some(f(&self.view))
+    }
+
+    fn with_view_mut<F, R>(&mut self, f: F) -> Option<R>
+    where
+        F: FnOnce(&mut Self::V) -> R,
+    {
+        Some(f(&mut self.view))
+    }
+
+    fn wrap_layout(&mut self, size: Vec2) {
+        self.text.if_changed(|t| self.content.set_content(t));
+        self.view.layout(size);
     }
 }
