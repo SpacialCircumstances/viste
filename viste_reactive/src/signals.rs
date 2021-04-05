30f4e3858edd6c13963b0f1e977fd75c840a8a17
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-04-05 15:15:41 +0200

    Traits for change tracking

--- viste_reactive/src/signals.rs
+++ viste_reactive/src/signals.rs
@@ -10,8 +10,22 @@ impl<T: Clone + PartialEq> Data for T {
     }
 }
 
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Epoch(usize);
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Id(usize);
+
+pub struct Retriever<'a, T: Data>(&'a dyn Fn() -> T);
+
+pub struct Transaction {
+    changed_signal_ids: Vec<Id>,
+}
+
 pub trait SignalCore<T: Data> {
-    fn get_data(&self) -> T;
+    fn get_data(&self) -> (Epoch, Retriever<T>);
+    fn mark_changed(&self, transaction: &mut Transaction);
+    fn get_id(&self) -> Id;
 }
 
 pub struct Signal<T: Data>(Rc<dyn SignalCore<T>>);
