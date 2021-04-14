bb37d96e142ca4b9ad9b934c4bbd796911c1eeb4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-04-14 23:12:09 +0200

    Start new signal implementation

--- viste_reactive/src/signals.rs
+++ viste_reactive/src/signals.rs
@@ -1,22 +1,94 @@
+use crate::old::graph::{Graph, NodeIndex, SearchContinuation};
 use crate::Data;
+use std::cell::{Ref, RefCell};
 use std::rc::Rc;
 
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
-pub struct Epoch(usize);
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct Generation(usize);
 
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
-pub struct Id(usize);
-
-pub struct Retriever<'a, T: Data>(&'a dyn Fn() -> T);
-
-pub struct Transaction {
-    changed_signal_ids: Vec<Id>,
+impl Generation {
+    pub fn incr(&mut self) {
+        self.0 = self.0 + 1
+    }
 }
 
-pub trait SignalCore<T: Data> {
-    fn get_data(&self) -> (Epoch, Retriever<T>);
-    fn mark_changed(&self, transaction: &mut Transaction);
-    fn get_id(&self) -> Id;
+struct WorldData {
+    dependencies: Graph<bool>,
+    current_generation: Generation,
 }
 
-pub struct Signal<T: Data>(Rc<dyn SignalCore<T>>);
+pub struct World(Rc<RefCell<WorldData>>);
+
+impl World {
+    pub fn new() -> Self {
+        World(Rc::new(RefCell::new(WorldData {
+            dependencies: Graph::new(),
+            current_generation: Generation(0),
+        })))
+    }
+
+    pub fn advance(&self) {
+        self.0.borrow_mut().current_generation.incr()
+    }
+
+    pub fn mark_dirty(&self, node: NodeIndex) {
+        let mut wd = self.0.borrow_mut();
+        let old_dirty = wd.dependencies[node];
+        if !old_dirty {
+            wd.dependencies.search_children_mut(
+                |child| {
+                    if !*child {
+                        *child = true;
+                        SearchContinuation::Continue
+                    } else {
+                        SearchContinuation::Stop
+                    }
+                },
+                node,
+            );
+        }
+    }
+
+    pub fn is_dirty(&self, node: NodeIndex) -> bool {
+        let wd = self.0.borrow();
+        wd.dependencies[node]
+    }
+
+    pub fn unmark(&self, node: NodeIndex) {
+        self.0.borrow_mut().dependencies[node] = false;
+    }
+
+    pub fn destroy_node(&self, node: NodeIndex) {
+        self.0.borrow_mut().dependencies.remove_node(node);
+    }
+
+    pub fn add_dependency(&self, parent: NodeIndex, child: NodeIndex) {
+        self.0.borrow_mut().dependencies.add_edge(parent, child);
+    }
+
+    pub fn remove_dependency(&self, parent: NodeIndex, child: NodeIndex) {
+        let mut wd = self.0.borrow_mut();
+        wd.dependencies.remove_edge(parent, child);
+    }
+}
+
+impl Default for World {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Clone for World {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+pub enum ComputationResult<T> {
+    Changed(T),
+    Unchanged,
+}
+
+pub trait Signal<T: Data> {
+    fn compute(&self, since: Generation) -> ComputationResult<T>;
+}
