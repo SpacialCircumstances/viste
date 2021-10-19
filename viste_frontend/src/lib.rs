45d5d5dfa997a468829eb1df906bff95cd53fd6b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-10-19 20:59:31 +0200

    Model reactive DOM

--- viste_frontend/src/lib.rs
+++ viste_frontend/src/lib.rs
@@ -1,10 +1,24 @@
 use quetta::Text;
+use viste_reactive::collections::VecView;
 use viste_reactive::*;
 use web_sys::*;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Attribute(Text, ValueSignal<'static, Text>);
+
+impl Attribute {
+    pub fn new(key: Text, value: ValueSignal<'static, Text>) -> Self {
+        Self(key, value)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum El {
     Text(Text),
+    Element(
+        VecView<'static, Attribute>,
+        VecView<'static, ValueSignal<'static, El>>,
+    ),
 }
 
 pub struct Component<Params>(Box<dyn Fn(Params) -> ValueSignal<'static, El>>);
