59f19f41e98df2c4d7106557dcaacb6e3600be8f
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-10-19 20:50:51 +0200

    Get basic web app working

---
+++ examples/frontend/hello/src/main.rs
@@ -0,0 +1,17 @@
+use quetta::Text;
+use viste_frontend::*;
+use viste_reactive::*;
+use web_sys::*;
+
+fn main() {
+    let world = World::new();
+    let window = window().expect("Where is my window?");
+    let document = window.document().expect("Where is my document?");
+    let main_element = document
+        .get_element_by_id("app")
+        .expect("Where is my element?");
+    render(
+        Component::new(move |()| constant(&world, El::Text(Text::new("Hello World!")))),
+        main_element,
+    )
+}
--- viste_frontend/src/lib.rs
+++ viste_frontend/src/lib.rs
@@ -1,1 +1,18 @@
+use quetta::Text;
+use viste_reactive::*;
+use web_sys::*;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum El {
+    Text(Text),
+}
+
+pub struct Component<Params>(Box<dyn Fn(Params) -> ValueSignal<'static, El>>);
+
+impl<Params> Component<Params> {
+    pub fn new<F: Fn(Params) -> ValueSignal<'static, El> + 'static>(render: F) -> Self {
+        Self(Box::new(render))
+    }
+}
+
+pub fn render(main: Component<()>, to: Element) {}
